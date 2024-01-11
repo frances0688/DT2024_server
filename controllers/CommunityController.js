164e93041aa3d1a4aa09f25ef7427c0985010772
Cocera <alcocera@gmail.com>
2024-01-11 16:54:27 +0100

    getAll => CommunityController

--- controllers/CommunityController.js
+++ controllers/CommunityController.js
@@ -11,8 +11,25 @@ const CommunityController = {
             });
             res.status(201).send({ message: "Comunidad creada con éxito" })
         } catch (error) {
+            console.error(error);
             res.status(500).send({ message: "No se ha podido crear la comunidad" })
-        }
+        };
+    },
+    async getAll(req, res) {
+        try {
+            const { page = 1, limit = 10 } = req.query;
+            const communities = await Community.find()
+                .limit(limit)
+                .skip((page - 1) * limit)
+                .populate({
+                    path: "incidences",
+                    select: "status category description"
+                });
+            res.status(201).send(communities);
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ message: "Error al acceder a las comunidades" })
+        };
     },
     async delete(req, res) {
         try {
@@ -22,8 +39,9 @@ const CommunityController = {
             };
             await Incidence.deleteMany({ community: req.params._id });
         } catch (error) {
+            console.error(error);
             res.status(500).send({ message: "No se ha podido eliminar la comunidad" })
-        }
+        };
     }
 };
 
